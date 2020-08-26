b55ecb92a11b111b3c07805bcfcfd1c7c4e022b1
Raymond <raymond.moorhead@gmail.com>
2020-08-26 10:48:45 -0700

    Created DollarsBank-back-end project Filled basic MySQL dependencies

---
+++ DollarsBank-back-end/src/main/java/com/dollarsbank/DollarsBankbackend/DollarsBankBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.dollarsbank.DollarsBankbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DollarsBankBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DollarsBankBackEndApplication.class, args);
+	}
+
+}
---
+++ DollarsBank-back-end/src/test/java/com/dollarsbank/DollarsBankbackend/DollarsBankBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dollarsbank.DollarsBankbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DollarsBankBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
