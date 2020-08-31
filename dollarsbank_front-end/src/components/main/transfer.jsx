50ec2c0e969d98d57eee037447d5f200d1cd878e
Matthew Stiller <mstiller@ggc.edu>
2020-08-31 10:17:23 -0400

    Use React state hook to set transfer destination. - Convert transfer.jsx to functional component. - Minor reformatting.

--- dollarsbank_front-end/src/components/main/transfer.jsx
+++ dollarsbank_front-end/src/components/main/transfer.jsx
@@ -1,60 +1,84 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-export default class Transfer extends Component {
+export default function Transfer() {
+  const [destination, setDestination] = useState(null);
 
-    render() {
+  return (
+    <div className="customerMain">
+      <div>
+        <h3>Transfer</h3>
 
-        return(
-            <div className="customerMain">
-                <div>
-                    <h3>Transfer</h3>
-                            
-                    {/* <div class="error">{ this.state.errorMsg }</div> */}
-                            
-                    <form action="" method="post" class="form">
-                    
-                        <div>
-                            <label for="account_from">Transfer From </label>
-                            <select name="account_from">
-                                <option value="">--Please choose an option--</option>
-                                <option value="savings">Savings</option>
-                                <option value="checking">Checking</option>
-                            </select>
-                        </div>
-                        <div>
-                            <label for="account_to">Transfer To </label>
-                            <select id="selection" name="account_to" onchange="other()">
-                                <option value="">--Please choose an option--</option>
-                                <option value="savings">Savings</option>
-                                <option value="checking">Checking</option>
-                                <option value="other">Another Customer</option>
-                            </select>
-                        </div>
-                        {/* State would be needed to hide or show option below */}
-                        <div id="another">
-                            <label for="other_account">To Account Id </label>
-                            <input type="number" name="other_account" min="0" step="1" placeholder="account id"/>
-                            <p></p>
-                            <label for="phone">Phone of Other Customer </label>
-                            <input type="text" name="phone" placeholder="phone"/>
-                        </div>
-                        <div>
-                            <label for="transaction_amount">Amount </label>
-                            <input type="number" name="transaction_amount" required placeholder="0.00" min="0.00" step="0.01"/>
-                        </div>
-                        <div>
-                            <p class="date">Date: <input type="text" id="date" readonly placeholder=""></input></p>
-                        </div>
-                        
-                        <div>
-                            <button type="submit" class="btn">Submit</button>
-                            <Link to="/overview" className="cancel">Cancel</Link>
-                            {/* <button type="button" name="cancel" class="cancel" onclick="history.back()">Cancel</button> */}
-                        </div>
-                    </form>
-                </div>
+        {/* <div class="error">{ this.state.errorMsg }</div> */}
+
+        <form action="" method="post" class="form">
+          <div>
+            <label for="account_from">Transfer From </label>
+            <select name="account_from">
+              <option value="">--Please choose an option--</option>
+              <option value="savings">Savings</option>
+              <option value="checking">Checking</option>
+            </select>
+          </div>
+          <div>
+            <label for="account_to">Transfer To </label>
+            <select
+              id="selection"
+              name="account_to"
+              value={destination}
+              onChange={(v) => setDestination(v.target.value)}
+            >
+              <option value="">--Please choose an option--</option>
+              <option value="savings">Savings</option>
+              <option value="checking">Checking</option>
+              <option value="other">Another Customer</option>
+            </select>
+          </div>
+          {/* State would be needed to hide or show option below */}
+          {destination === "other" && (
+            <div id="another">
+              <label for="other_account">To Account Id </label>
+              <input
+                type="number"
+                name="other_account"
+                min="0"
+                step="1"
+                placeholder="account id"
+              />
+              <p></p>
+              <label for="phone">Phone of Other Customer </label>
+              <input type="text" name="phone" placeholder="phone" />
             </div>
-        )
-    }
+          )}
+          <div>
+            <label for="transaction_amount">Amount </label>
+            <input
+              type="number"
+              name="transaction_amount"
+              required
+              placeholder="0.00"
+              min="0.00"
+              step="0.01"
+            />
+          </div>
+          <div>
+            <p class="date">
+              Date:{" "}
+              <input type="text" id="date" readonly placeholder=""></input>
+            </p>
+          </div>
+
+          <div>
+            <button type="submit" class="btn">
+              Submit
+            </button>
+            <Link to="/overview" className="cancel">
+              Cancel
+            </Link>
+            {/* <button type="button" name="cancel" class="cancel" onclick="history.back()">Cancel</button> */}
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
