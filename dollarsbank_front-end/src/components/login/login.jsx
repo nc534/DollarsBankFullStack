21d247a50b509ce4a926de02bd87409811fbac82
Matthew Stiller <mstiller@ggc.edu>
2020-08-31 14:53:53 -0400

    Cause login.jsx to handle user entry. - converted to functional component. - utilize useState hooks for input changes. - user entries are stored in-memory for later login credential check.

--- dollarsbank_front-end/src/components/login/login.jsx
+++ dollarsbank_front-end/src/components/login/login.jsx
@@ -1,39 +1,62 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
-import './style.css';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
 
-export default class Login extends Component {
+export default function Login() {
+  const [username, setUsername] = useState(null);
+  const [password, setPassword] = useState(null);
 
-    render() {
-        return (
-            <div class="main">
-            <h1>DOLLARSBANK Welcomes You!!</h1>
-                <div class="container">
-                    <div class="header">
-                        Login
-                    </div>
-                    
-                    {/* <div class="error">{ this.state.errorMsg }</div>
+  function handleLogin(event) {
+    // TODO use the state vars to send a REST API request.
+    event.preventDefault();
+    alert("Username " + username + "\nPassword: " + password);
+  }
+
+  return (
+    <div class="main">
+      <h1>DOLLARSBANK Welcomes You!!</h1>
+      <div class="container">
+        <div class="header">Login</div>
+
+        {/* <div class="error">{ this.state.errorMsg }</div>
                     <div class="message">{this.state.message}</div> */}
-                    
-                    <form action=/*{this.handleLogin}*/"" method="post" class="form-main">
-                        <div class="form-group">
-                            <label for="username">Username</label>
-                            <input class="input" type="text" name="username" required placeholder="username" /*value={this.state.username} onChange={this.handleChange}*//>
-                        </div>
-                        <div class="form-group">
-                            <label for="password">Password</label>
-                            <input class="input" type="password" name="password" required placeholder="password" /*value={this.state.username} onChange={this.handleChange}*//>
-                        </div>
-                        <div>
-                            <button type="submit" class="btn">Login</button>
-                        </div>
-                        <div>
-                            <p>Don't have an account? </p><Link to='/register'><p>Register</p></Link>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        )
-    }
+
+        <form onSubmit={handleLogin} class="form-main">
+          <div class="form-group">
+            <label for="username">Username</label>
+            <input
+              class="input"
+              type="text"
+              name="username"
+              required
+              placeholder="username"
+              onChange={(v) => setUsername(v.target.value)}
+            />
+          </div>
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input
+              class="input"
+              type="password"
+              name="password"
+              required
+              placeholder="password"
+              onChange={(v) => setPassword(v.target.value)}
+            />
+          </div>
+          <div>
+            <button type="submit" class="btn">
+              Login
+            </button>
+          </div>
+          <div>
+            <p>Don't have an account? </p>
+            <Link to="/register">
+              <p>Register</p>
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
