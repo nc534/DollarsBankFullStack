da160ebd0f51fe298a10ec54e7c2f3427684dc17
Abdulbasit Raza <Abdul@Abdulbasits-MBP.home>
2020-09-01 09:19:25 -0400

    controller

---
+++ DollarsBank-back-end/src/main/java/com/dollarsbank/DollarsBankbackend/controller/AccountController.java
@@ -0,0 +1,17 @@
+package com.dollarsbank.DollarsBankbackend.controller;
+
+
+import com.dollarsbank.DollarsBankbackend.Service.TestAccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class AccountController {
+    @Autowired
+    private TestAccountService AccountService;
+
+    @RequestMapping(method = RequestMethod.POST, value = "/addacct")
+    public @ResponseBody String addAcct(@ResponseBody String enteredString) throws Exception {
+        return AccountService.AcctAdd(enteredString);
+    }
+}
---
+++ DollarsBank-back-end/src/main/java/com/dollarsbank/DollarsBankbackend/controller/CustomerController.java
@@ -0,0 +1,35 @@
+package com.dollarsbank.DollarsBankbackend.controller;
+
+import java.util.ArrayList;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dollarsbank.DollarsBankbackend.Service.TestCustomerService;
+
+@RestController
+public class CustomerController {
+
+    @Autowired
+    private TestCustomerService CustomerService;
+
+    @RequestMapping(method = RequestMethod.POST, value = "/login")
+    public boolean loginClicked(@RequestBody String enteredString) throws Exception {
+        System.out.println(enteredString);
+        return CustomerService.loginUser(enteredString);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/signup")
+    public string signup(@RequestBody String enteredString) throws Exception {
+        System.out.println(enteredString);
+        return CustomerService.makeUser(enteredString);
+    }
+}
