51cfae26bf7e93cea5d546d76cd595bcf1c6e41a
Matthew Stiller <mstiller@ggc.edu>
2020-09-01 13:23:44 -0400

    Implement initial user registration. - utilize Axios and URL constant to direct data to DB endpoint - register.jsx bundles user input into JSON transmission format.

--- dollarsbank_front-end/src/components/login/register.jsx
+++ dollarsbank_front-end/src/components/login/register.jsx
@@ -1,8 +1,35 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
+import * as CONSTANTS from "../constants";
+import axios from "axios";
 
 export default function Register() {
+  const url = CONSTANTS.API;
+
+  const [username, setUsername] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [name, setName] = useState(null);
+  const [address, setAddress] = useState(null);
+  const [phone, setPhone] = useState(null);
+  const [accountType, setAccountType] = useState(null);
+  const [accountBalance, setAccountBalance] = useState(null);
+
+  async function handleRegister(event) {
+    event.preventDefault();
+    // TODO use the state vars to send a REST API request.
+    const res = await axios.post(url + "/signup", {
+      username: username,
+      password: password,
+      name: name,
+      address: address,
+      phone: phone,
+      accountType: accountType,
+      accountBalance: accountBalance,
+    });
+    console.log(res);
+  }
+
   return (
     <div class="main">
       <div class="container">
@@ -10,10 +37,13 @@ export default function Register() {
 
         {/* <div class="error">{ this.state.errorMsg }</div>*/}
 
-        <form action="/*{this.handleLogin}*/" method="post" class="form-main">
+        <form onSubmit={handleRegister} class="form-main">
           <div class="form-group">
             <label for="account">Account </label>
-            <select name="account">
+            <select
+              name="account"
+              onChange={(v) => setAccountType(v.target.value)}
+            >
               <option value="">--Please choose an option--</option>
               <option value="savings">Savings</option>
               <option value="checking">Checking</option>
@@ -29,6 +59,7 @@ export default function Register() {
               placeholder="0.00"
               min="0.00"
               step="0.01"
+              onChange={(v) => setAccountBalance(v.target.value)}
             />
           </div>
           <div class="form-group">
@@ -39,6 +70,7 @@ export default function Register() {
               name="name"
               required
               placeholder="name" /*value={this.state.name} onChange={this.handleChange}*/
+              onChange={(v) => setName(v.target.value)}
             />
           </div>
           <div class="form-group">
@@ -49,6 +81,7 @@ export default function Register() {
               name="address"
               required
               placeholder="address" /*value={this.state.address} onChange={this.handleChange}*/
+              onChange={(v) => setAddress(v.target.value)}
             />
           </div>
           <div class="form-group">
@@ -59,6 +92,7 @@ export default function Register() {
               name="phone"
               required
               placeholder="phone" /*value={this.state.phone} onChange={this.handleChange}*/
+              onChange={(v) => setPhone(v.target.value)}
             />
           </div>
           <div class="form-group">
@@ -69,6 +103,7 @@ export default function Register() {
               name="username"
               required
               placeholder="username" /*value={this.state.username} onChange={this.handleChange}*/
+              onChange={(v) => setUsername(v.target.value)}
             />
           </div>
           <div class="form-group">
@@ -82,6 +117,7 @@ export default function Register() {
               pattern="(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}"
               title="Must contain at least one number, one uppercase, one lowercase letter, and at least 8 or more characters"
               /*value={this.state.password} onChange={this.handleChange}*/
+              onChange={(v) => setPassword(v.target.value)}
             />
           </div>
           <button type="submit" class="btn">
