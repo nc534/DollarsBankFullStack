28bcec9b0ca2730230af5971c1fe84225dd78e9e
Matthew Stiller <mstiller@ggc.edu>
2020-09-01 12:30:24 -0400

    Convert register.jsx to functional component.

--- dollarsbank_front-end/src/components/login/register.jsx
+++ dollarsbank_front-end/src/components/login/register.jsx
@@ -1,102 +1,100 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
 
-export default class Register extends Component {
-  render() {
-    return (
-      <div class="main">
-        <div class="container">
-          <div class="header">Register</div>
+export default function Register() {
+  return (
+    <div class="main">
+      <div class="container">
+        <div class="header">Register</div>
 
-          {/* <div class="error">{ this.state.errorMsg }</div>*/}
+        {/* <div class="error">{ this.state.errorMsg }</div>*/}
 
-          <form action="/*{this.handleLogin}*/" method="post" class="form-main">
-            <div class="form-group">
-              <label for="account">Account </label>
-              <select name="account">
-                <option value="">--Please choose an option--</option>
-                <option value="savings">Savings</option>
-                <option value="checking">Checking</option>
-              </select>
-            </div>
-            <div class="form-group">
-              <label for="initial_deposit">Amount</label>
-              <input
-                class="input"
-                type="number"
-                name="initial_deposit"
-                required
-                placeholder="0.00"
-                min="0.00"
-                step="0.01"
-              />
-            </div>
-            <div class="form-group">
-              <label for="name">Name</label>
-              <input
-                class="input"
-                type="text"
-                name="name"
-                required
-                placeholder="name" /*value={this.state.name} onChange={this.handleChange}*/
-              />
-            </div>
-            <div class="form-group">
-              <label for="address">Address</label>
-              <input
-                class="input"
-                type="text"
-                name="address"
-                required
-                placeholder="address" /*value={this.state.address} onChange={this.handleChange}*/
-              />
-            </div>
-            <div class="form-group">
-              <label for="phone">Phone</label>
-              <input
-                class="input"
-                type="text"
-                name="phone"
-                required
-                placeholder="phone" /*value={this.state.phone} onChange={this.handleChange}*/
-              />
-            </div>
-            <div class="form-group">
-              <label for="username">Username</label>
-              <input
-                class="input"
-                type="text"
-                name="username"
-                required
-                placeholder="username" /*value={this.state.username} onChange={this.handleChange}*/
-              />
-            </div>
-            <div class="form-group">
-              <label for="password">Password</label>
-              <input
-                class="input"
-                type="password"
-                name="password"
-                required
-                placeholder="password"
-                pattern="(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}"
-                title="Must contain at least one number, one uppercase, one lowercase letter, and at least 8 or more characters"
-                /*value={this.state.password} onChange={this.handleChange}*/
-              />
-            </div>
-            <button type="submit" class="btn">
-              Register
-            </button>
-            <div>
-              <p>Already have an account?</p>
-              <Link to="/login">
-                <p>Login</p>
-              </Link>
-            </div>
-          </form>
-        </div>
+        <form action="/*{this.handleLogin}*/" method="post" class="form-main">
+          <div class="form-group">
+            <label for="account">Account </label>
+            <select name="account">
+              <option value="">--Please choose an option--</option>
+              <option value="savings">Savings</option>
+              <option value="checking">Checking</option>
+            </select>
+          </div>
+          <div class="form-group">
+            <label for="initial_deposit">Amount</label>
+            <input
+              class="input"
+              type="number"
+              name="initial_deposit"
+              required
+              placeholder="0.00"
+              min="0.00"
+              step="0.01"
+            />
+          </div>
+          <div class="form-group">
+            <label for="name">Name</label>
+            <input
+              class="input"
+              type="text"
+              name="name"
+              required
+              placeholder="name" /*value={this.state.name} onChange={this.handleChange}*/
+            />
+          </div>
+          <div class="form-group">
+            <label for="address">Address</label>
+            <input
+              class="input"
+              type="text"
+              name="address"
+              required
+              placeholder="address" /*value={this.state.address} onChange={this.handleChange}*/
+            />
+          </div>
+          <div class="form-group">
+            <label for="phone">Phone</label>
+            <input
+              class="input"
+              type="text"
+              name="phone"
+              required
+              placeholder="phone" /*value={this.state.phone} onChange={this.handleChange}*/
+            />
+          </div>
+          <div class="form-group">
+            <label for="username">Username</label>
+            <input
+              class="input"
+              type="text"
+              name="username"
+              required
+              placeholder="username" /*value={this.state.username} onChange={this.handleChange}*/
+            />
+          </div>
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input
+              class="input"
+              type="password"
+              name="password"
+              required
+              placeholder="password"
+              pattern="(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}"
+              title="Must contain at least one number, one uppercase, one lowercase letter, and at least 8 or more characters"
+              /*value={this.state.password} onChange={this.handleChange}*/
+            />
+          </div>
+          <button type="submit" class="btn">
+            Register
+          </button>
+          <div>
+            <p>Already have an account?</p>
+            <Link to="/login">
+              <p>Login</p>
+            </Link>
+          </div>
+        </form>
       </div>
-    );
-  }
+    </div>
+  );
 }
