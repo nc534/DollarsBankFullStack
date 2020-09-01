855d01de8941c86e1e0bb3661f1535478610424b
Matthew Stiller <mstiller@ggc.edu>
2020-09-01 15:41:27 -0400

    Rename constants.jsx -> Model.jsx. - store constants and functions in one global location.

---
+++ dollarsbank_front-end/src/components/Model.jsx
@@ -0,0 +1,8 @@
+// URL endpoint for database server
+import axios from "axios";
+
+export const API = "http://localhost:8080";
+
+export async function login(customer) {
+  // TODO logic for updating the global application state upon successful login.
+}
