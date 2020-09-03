3394dba2925ceb4afe9834e15e23436d724a9a39
Matthew Stiller <mstiller@ggc.edu>
2020-09-03 13:50:33 -0400

    Quell HTML <tr> errors.

--- dollarsbank_front-end/src/components/main/customer.jsx
+++ dollarsbank_front-end/src/components/main/customer.jsx
@@ -3,25 +3,27 @@ import { AppContext } from "../App";
 
 export default function Customer() {
   const state = useContext(AppContext);
-  
+
   return (
     <div className="customerMain">
       <div className="accountInfo">
         <h3>Your Account Information</h3>
 
         <table>
-          <tr>
-            <td>Name: {state.user.name}</td>
-          </tr>
-          <tr>
-            <td>Phone: {state.user.contactNumber}</td>
-          </tr>
-          <tr>
-            <td>Address: {state.user.address}</td>
-          </tr>
-          <tr>
-            <td>User Id: {state.user.username}</td>
-          </tr>
+          <tbody>
+            <tr>
+              <td>Name: {state.user.name}</td>
+            </tr>
+            <tr>
+              <td>Phone: {state.user.contactNumber}</td>
+            </tr>
+            <tr>
+              <td>Address: {state.user.address}</td>
+            </tr>
+            <tr>
+              <td>User Id: {state.user.username}</td>
+            </tr>
+          </tbody>
         </table>
       </div>
     </div>
