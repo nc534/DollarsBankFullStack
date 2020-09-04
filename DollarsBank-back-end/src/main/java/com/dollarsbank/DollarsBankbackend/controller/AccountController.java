02a700726495ff4ef3e2abcbbb736d7e1a566fdb
Abdulbasit Raza <Abdul@Abdulbasits-MBP.home>
2020-09-04 09:47:28 -0400

    master

--- DollarsBank-back-end/src/main/java/com/dollarsbank/DollarsBankbackend/controller/AccountController.java
+++ DollarsBank-back-end/src/main/java/com/dollarsbank/DollarsBankbackend/controller/AccountController.java
@@ -2,16 +2,29 @@ package com.dollarsbank.DollarsBankbackend.controller;
 
 
 import com.dollarsbank.DollarsBankbackend.Service.TestAccountService;
+import com.dollarsbank.DollarsBankbackend.model.Account;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 public class AccountController {
     @Autowired
     private TestAccountService AccountService;
 
-    @RequestMapping(method = RequestMethod.POST, value = "/addacct")
-    public @ResponseBody String addAcct(String enteredString) throws Exception {
-        return AccountService.AcctAdd(enteredString);
+    @PostMapping("/addacct")
+    public ResponseEntity<Account> AcctAdd(@RequestBody Account account) throws Exception {
+        Account newAcct = AccountService.AcctAdd(account);
+        if(newAcct == null) {
+            return ResponseEntity.notFound().build();
+        }
+        else {
+            URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newAcct.getCustId().toUri();
+            return ResponseEntity.created(uri).body(newAcct);
+
+        }
     }
 }
