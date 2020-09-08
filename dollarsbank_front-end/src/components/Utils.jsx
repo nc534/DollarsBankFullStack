fa3403b2d4e6a3b634c216fd2a471939cdb2796b
Matthew Stiller <mstiller@ggc.edu>
2020-09-08 15:17:19 -0400

    Add account creation POST method. - touchup Utils logic. - add endpoints const array.

--- dollarsbank_front-end/src/components/Utils.jsx
+++ dollarsbank_front-end/src/components/Utils.jsx
@@ -2,21 +2,44 @@ import axios from "axios";
 
 // ! URL endpoint for database server
 const API = "http://localhost:8080";
+const endpoints = {
+  register: "/signup",
+  login: "/login",
+  account_create: "/addacct",
+};
 
 export async function register(user) {
-  const res = await axios.post(API + "/signup", user);
+  const res = await axios.post(API + endpoints.register, user);
   // If the user could not be created, return false.
   if (res.data.id === 0) return false;
   return res.data;
 }
 
 export async function login(user) {
-  // TODO logic for updating the global application state upon successful login.
-  const res = await axios.post(API + "/login", user);
+  const res = await axios.post(API + endpoints.login, user);
+  // If the login credentials are invalid, return false.
+  if (res.data === "") return false;
   return res.data;
 }
 
 export function logout() {
   // TODO logout logic.
   return null;
+}
+
+/**
+ * POSTs an Account object to the server.
+ * @param {*} account The account to be sent.
+ * @returns The created account. If already exists, returns with null values.
+ */
+export async function createAccount(account) {
+  const res = await axios.post(API + endpoints.account_create, account);
+  // TODO verification logic.
+  // If the account could not be created, return false.
+  if (res.data.id === 0) return false;
+  return res.data;
+}
+
+export async function getAccounts() {
+  // TODO when endpoint exists...
 }
--- dollarsbank_front-end/src/components/login/login.jsx
+++ dollarsbank_front-end/src/components/login/login.jsx
@@ -18,7 +18,7 @@ export default function Login() {
       password: password,
     });
 
-    if (res === "") {
+    if (!res) {
       changeMsg();
     } else {
       state.setUser(res);
