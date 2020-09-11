96f2c50819d3cc4cc6c0a2a41fbaf20bb74d450f
Matthew Stiller <mstiller@ggc.edu>
2020-09-11 11:53:11 -0400

    Cleanup transfer.

--- dollarsbank_front-end/src/components/main/transfer.jsx
+++ dollarsbank_front-end/src/components/main/transfer.jsx
@@ -1,24 +1,21 @@
 import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import { AppContext } from "../App";
-import Select from 'react-select';
+import Select from "react-select";
 
 export default function Transfer() {
   const [sourceAccId, setSource] = useState(undefined);
   const [targetAccId, setDestination] = useState(undefined);
   const [memo, setMemo] = useState(undefined);
   const [state] = useContext(AppContext);
-  const accounts = state.accounts.map(a => ({
-    "value": a.id,
-    "label": a.accountName
+  const accounts = state.accounts.map((a) => ({
+    value: a.id,
+    label: a.accountName,
   }));
 
   let today = new Date();
   let date =
-    (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
-  // let time =
-  //   today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-  // let dateTime = date + " " + time;
+    today.getMonth() + 1 + "/" + today.getDate() + "/" + today.getFullYear();
 
   return (
     <div className="customerMain">
@@ -32,10 +29,10 @@ export default function Transfer() {
         <form action="" method="post" className="form">
           <div className="form-group">
             <label htmlFor="account_from">Transfer From </label>
-            <Select 
-              name="account_from" 
+            <Select
+              name="account_from"
               className="select"
-              options={accounts} 
+              options={accounts}
               onChange={(v) => setSource(v.value)}
             />
           </div>
@@ -46,11 +43,13 @@ export default function Transfer() {
             <Select
               name="account_to"
               className="select"
-              options={accounts.concat({"value": "other", "label":"Another Customer"})}
+              options={accounts.concat({
+                value: "other",
+                label: "Another Customer",
+              })}
               onChange={(v) => setDestination(v.value)}
             />
           </div>
-          {/* State would be needed to hide or show option below */}
           {targetAccId === "other" && (
             <div id="another" className="form-group">
               <label htmlFor="targetAccId">To Account Id </label>
@@ -79,11 +78,25 @@ export default function Transfer() {
           </div>
           <div className="form-group">
             <label htmlFor="memo">Note </label>
-            <input type="text" name="memo" className="memo" value={memo} required placeholder="e.g. borrowed money" onChange={(v) => setMemo(v.target.value)}/>
+            <input
+              type="text"
+              name="memo"
+              className="memo"
+              value={memo}
+              required
+              placeholder="e.g. borrowed money"
+              onChange={(v) => setMemo(v.target.value)}
+            />
           </div>
           <div className="date form-group">
-              <label htmlFor="date">Date </label>
-              <input type="text" name="date" id="date" readOnly placeholder={date}></input>
+            <label htmlFor="date">Date </label>
+            <input
+              type="text"
+              name="date"
+              id="date"
+              readOnly
+              placeholder={date}
+            ></input>
           </div>
 
           <div className="form-group">
