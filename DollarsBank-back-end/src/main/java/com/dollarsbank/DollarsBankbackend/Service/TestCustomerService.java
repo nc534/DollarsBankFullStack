bfa807ed05af5112c16db31af198999504a31c82
Abdulbasit Raza <Abdul@Abdulbasits-MBP.home>
2020-09-14 15:55:04 -0400

    cleaning backend code

--- DollarsBank-back-end/src/main/java/com/dollarsbank/DollarsBankbackend/Service/TestCustomerService.java
+++ DollarsBank-back-end/src/main/java/com/dollarsbank/DollarsBankbackend/Service/TestCustomerService.java
@@ -17,17 +17,7 @@ public class TestCustomerService {
     private AccountRepository accRepo;
 
     /************************* User Login ****************************/
-//<<<<<<< HEAD
-//    public boolean loginUser(String username, String password) {
-//
-//
-//        String truePassword = ValidationUtility.generatePassword(username, password);
-//
-//        return custRepo.existsByUsernameAndPassword(username, truePassword);
-//
-//
-//
-//
+
     public Customer loginUser(String username, String password) {
         String truePassword = ValidationUtility.generatePassword(username, password);
 
@@ -37,31 +27,7 @@ public class TestCustomerService {
 
     /******************************* Create User ******************************/
 
-//<<<<<<< HEAD
-//    public String makeUser(String enteredString) {
-//        enteredString = enteredString.substring(1, enteredString.length()-1);
-//        String delim = "/n";
-//        String[] onlyValues = enteredString.split(delim);
-//
-//        Customer newCustomer = new Customer();
-//        int numAdded = 0;
-//
-//
-//            if(!custRepo.existsByUsername(onlyValues[0])) {
-//                newCustomer.setId(0);
-//                newCustomer.setUsername(onlyValues[0]);
-//                newCustomer.setNewPassword(onlyValues[1]);
-//                newCustomer.setName(onlyValues[2]);
-//                newCustomer.setAddress(onlyValues[3]);
-//                newCustomer.setContactNumber(onlyValues[4]);
-//                newCustomer = custRepo.save(newCustomer);
-//                if(newCustomer != null){
-//                    ++numAdded;
-//                }
-//            }
-//
-//        return numAdded + " accounts have been added";
-//=======
+
     public Customer makeUser(Customer customer) {
 
 
